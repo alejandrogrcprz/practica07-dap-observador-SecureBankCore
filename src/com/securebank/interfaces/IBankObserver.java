2d513e801483fa833caa38060f2a68fd54965e86
Alejandro García Pérez <alu0101441207@ull.edu.es>
2025-12-30 16:48:47 +0000

    refactor: eliminar estructura de paquetes antigua y limpiar archivos deprecados

--- src/com/securebank/interfaces/IBankObserver.java
+++
@@ -1,6 +0,0 @@
-package com.securebank.interfaces;
-
-public interface IBankObserver {
-  // El contrato que todos los observadores deben cumplir
-  void onTransactionExecuted(String transactionData);
-}
--- src/com/securebank/interfaces/ITransactionSubject.java
+++
@@ -1,7 +0,0 @@
-package com.securebank.interfaces;
-
-public interface ITransactionSubject {
-  void attach(IBankObserver observer); // Suscribir
-  void detach(IBankObserver observer); // Desuscribir
-  void notifyObservers(String data);   // Avisar a todos
-}
